6ac2a54a0a3dbb75a806e1a5cb7e1a3ef50017a2
EoinFitz03 <118372036+EoinFitz03@users.noreply.github.com>
2025-11-10 17:39:14 +0000

    full exam

--- app/models.py
+++ app/models.py
@@ -5,3 +5,16 @@ class Base(DeclarativeBase):
     pass
 
 
+class AuthorDB(Base):
+    __tablename__= "Authors"
+    id: Mapped[int]= mapped_column(primary_key=True)
+    name: Mapped[str]= mapped_column(String(100), nullable= False)
+    email: Mapped[str]= mapped_column(String(100), nullable= False)
+    year_started: Mapped[int] = mapped_column(unique=True, nullable = False)
+
+class BookDB(Base):
+    __tablename__= "Books"
+    id: Mapped[int]= mapped_column(primary_key=True)
+    title: Mapped[str]= mapped_column(String(100), nullable= False)
+    pages: Mapped[str]= mapped_column(String(100), nullable= False)
+    Owner_id: Mapped[int] = mapped_column(ForeignKey("authors.id",ondelete="CASCADE", nullable= False))
